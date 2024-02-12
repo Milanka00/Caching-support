afb07f63839a687bb59b5a6172d8d64691aa4b8d
Milanka00 <milankatharangana01@gmail.com>
2024-02-12 18:21:02 +0530

    general GET request

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue train", Artist: "john Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 18.99},
+	{ID: "3", Title: "Clifford Brown", Artist: "Sarah Vaughan", Price: 58.99},
+}
+
+// response in jason format
+func getAlbums(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK,albums)
+}
+
+func main() {
+ router := gin.Default()
+ router.GET("/albums",getAlbums)
+
+ router.Run("localhost:8080")
+}
