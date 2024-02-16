9439ce1fca4d87fe5be61ea32fb59e541b3360f3
Milanka00 <milankatharangana01@gmail.com>
2024-02-16 10:07:46 +0530

    react application

---
+++ front-end/cache/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [albums, setAlbums] = useState([]);
+
+  // useEffect(() => {
+  //   async function fetchAlbums() {
+  //     const response = await fetch('http://localhost:10000/albums');
+  //     const data = await response.json();
+  //     setAlbums(data);
+  //   }
+  //   fetchAlbums();
+  // }, []);
+
+  return (
+    <div>
+      <h1>Albums</h1>
+      {/* <ul>
+        {albums.map(album => (
+          <li key={album.id}>
+            {album.title} by {album.artist} - ${album.price}
+          </li>
+        ))}
+      </ul> */}
+    </div>
+  );
+}
+
+export default App;
