c6eb3cc2b8bc64d1ceac3a51f7ec70ea6899cb38
Milanka00 <milankatharangana01@gmail.com>
2024-03-08 09:25:08 +0530

    2 nd backend

---
+++ go_backend2/go.mod
@@ -0,0 +1,3 @@
+module go_backend2
+
+go 1.22.0
---
+++ go_backend2/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+    "strconv"
+    "sync"
+    "time"
+)
+
+var payload []byte
+var once sync.Once
+
+func main() {
+    
+    generatePayload()
+
+   
+    http.HandleFunc("/nocache", func(w http.ResponseWriter, r *http.Request) {
+        NoCacheHandler(w, r)
+    })
+    http.HandleFunc("/publiccache", func(w http.ResponseWriter, r *http.Request) {
+        PublicCacheHandler(w, r)
+    })
+   
+    http.HandleFunc("/getresponse", func(w http.ResponseWriter, r *http.Request) {
+        getresponseWithoutHeaders(w, r)
+    })
+
+    // Start server
+    fmt.Println("Server is listening on port 8081...")
+    http.ListenAndServe(":8082", nil)
+}
+
+
+func NoCacheHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Cache-Control", "no-store")
+    w.Write(payload)
+    sleepBeforeRespond()
+}
+
+
+func PublicCacheHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Cache-Control", "public, max-age=3600")
+    w.Write(payload)
+    sleepBeforeRespond()
+}
+
+func getresponseWithoutHeaders(w http.ResponseWriter, r *http.Request) {
+    w.Write(payload)
+    sleepBeforeRespond()
+}
+
+func sleepBeforeRespond() {
+    sleepTimeStr := os.Getenv("SLEEP_TIME")
+    sleepTime, err := strconv.Atoi(sleepTimeStr)
+    if err != nil {
+        sleepTime = 15
+    }
+    time.Sleep(time.Duration(sleepTime) * time.Second)
+}
+
+func generatePayload() {
+    once.Do(func() {
+        payload = make([]byte, 100)
+        for i := range payload {
+            payload[i] = 'y'
+        }
+    })
+}
