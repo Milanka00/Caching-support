bf6cc4d33ded852c2270cfcddaa2e1639595e3ec
Milanka00 <milankatharangana01@gmail.com>
2024-03-18 08:11:16 +0530

    other caching headers

--- backend_perId/main.go
+++ backend_perId/main.go
@@ -17,6 +17,18 @@ func main() {
 	http.HandleFunc("/publiccache/", func(w http.ResponseWriter, r *http.Request) {
 		PublicCacheHandler(w, r)
 	})
+	
+   
+    http.HandleFunc("/nocache", func(w http.ResponseWriter, r *http.Request) {
+        NoCacheHandler(w, r)
+    })
+
+    http.HandleFunc("/privatecache", func(w http.ResponseWriter, r *http.Request) {
+        PrivateCacheHandler(w, r)
+    })
+    http.HandleFunc("/getresponse", func(w http.ResponseWriter, r *http.Request) {
+        getresponseWithoutHeaders(w, r)
+    })
 
 	// Start server
 	fmt.Println("Server is listening on port 8081...")
@@ -42,10 +54,27 @@ func PublicCacheHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(additionalContent)
 }
 
+func PrivateCacheHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Cache-Control", "private, max-age=3600")
+   // w.Write(payload)
+    // sleepBeforeRespond()
+}
+
+func getresponseWithoutHeaders(w http.ResponseWriter, r *http.Request) {
+   // w.Write(payload)
+    // sleepBeforeRespond()
+}
+
+func NoCacheHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Cache-Control", "no-store")
+   // w.Write(payload)
+    // sleepBeforeRespond()
+}
+
 func generatePayloads() {
 	once.Do(func() {
 		payloads = make(map[int][]byte)
-		for i := 1; i <= 20; i++ {
+		for i := 1; i <= 50; i++ {
 			payload := make([]byte, 100)
 			for j := range payload {
 				payload[j] = 'x'
